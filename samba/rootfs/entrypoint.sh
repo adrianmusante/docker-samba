693e594e1f92e0dbb12db322b5564bd3bf6afdcb
Adrián Musante <adrianmusante@gmail.com>
2025-06-21 19:18:26 -0300

    update entrypoint.sh to include Samba version in startup message

--- samba/rootfs/entrypoint.sh
+++ samba/rootfs/entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 eval "$(samba-env)"
 
-info "Running Samba with build version: ${BUILD_TAG}"
+info "Running Samba v$SAMBA_VERSION with build version: ${BUILD_TAG}"
 debug "USER: $(id)"
 debug "HOSTNAME: $(hostname)"
 
